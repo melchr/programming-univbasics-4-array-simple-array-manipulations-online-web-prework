5ef7d1d062bf9b1c90d6522d0c6ac276cfc50494
Melissa Christman <melchrstmn@gmail.com>
2019-09-02 16:49:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,5 @@
 def using_push
   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
   next_color = ["black"]
+  updated_array = using_push(colors_in_the_rainbow, next_color)
 end
